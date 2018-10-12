0f8afb6252484a321e81b4e93c8417d795cdd8e7
Massimo Bernava <>
2018-10-12 11:09:11 +0200

    torch.range -> torch.linspace

--- torch/SpectralArcLength.lua
+++ torch/SpectralArcLength.lua
@@ -1,6 +1,8 @@
 
 signal = require 'signal'
 
+torch.setdefaulttensortype("torch.FloatTensor")
+
 -- Examples
 --
 -- t = torch.range(-1,1,0.01)
@@ -18,6 +20,7 @@ function SpectralArcLength(speed,fs,padlevel,fc,amp_th)
 		print("Error! Input at least the movement speed profile for the smoothness calculation")
 		return nil
 	end
+	
 	fs=fs or 1/100
 	padlevel=padlevel or 4
 	fc=fc or 10.0
@@ -26,10 +29,12 @@ function SpectralArcLength(speed,fs,padlevel,fc,amp_th)
 	local N=(#speed)[1]
 	-- Number of zeros to be padded.
 	local Nfft=torch.pow(2,torch.ceil(torch.log(N)/torch.log(2.0))+padlevel)
+
 	local speed_pad=torch.FloatTensor(Nfft):fill(0)
 	speed_pad:narrow(1,1,N):copy(speed)
 	-- Frequency
-	local freq=torch.range(0,fs,fs/(Nfft-1))
+	--local freq=torch.range(0,fs,fs/(Nfft-1))
+	local freq=torch.linspace(0,fs-(fs/Nfft),Nfft)
 	-- Normalized magnitude spectrum
 	local speedSpectrum=signal.complex.abs(signal.fft(speed_pad))
 	speedSpectrum=speedSpectrum/torch.max(speedSpectrum)
@@ -44,8 +49,11 @@ function SpectralArcLength(speed,fs,padlevel,fc,amp_th)
 	freq_sel=freq_sel[{{inx,inx[#inx]}}]
 	speedSpectrum_sel=speedSpectrum_sel[{{inx,inx[#inx]}}]
 
-	local new_sal=torch.sum(torch.sqrt(torch.pow(diff(freq_sel)/(freq_sel[(#freq_sel)[1]]-freq_sel[1]),2)
-				+torch.pow(diff(speedSpectrum_sel),2)))
+	local new_sal=torch.sum(torch.sqrt(torch.add(
+				torch.pow(diff(freq_sel)/(freq_sel[(#freq_sel)[1]]-freq_sel[1]),2)
+				,torch.pow(diff(speedSpectrum_sel),2)
+			)))
+			
 
 	return new_sal,{freq,speedSpectrum},{freq_sel,speedSpectrum_sel}
 end
